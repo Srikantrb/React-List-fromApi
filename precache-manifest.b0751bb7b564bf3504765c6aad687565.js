c97a5a1f9a3e7ae04dee73a86b1fb2f752e90f95
srikanth reddy badam <srikantrb@gmail.com>
2020-09-01 12:04:20 +0530

    Updates

---
+++ precache-manifest.b0751bb7b564bf3504765c6aad687565.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d473af357bfaee1d54cea9693db80734",
+    "url": "/React-slot-bookingApp/index.html"
+  },
+  {
+    "revision": "babd85d4ba6674e8b8f8",
+    "url": "/React-slot-bookingApp/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c9a78a6adcb37c906231",
+    "url": "/React-slot-bookingApp/static/css/main.6ca82ca7.chunk.css"
+  },
+  {
+    "revision": "babd85d4ba6674e8b8f8",
+    "url": "/React-slot-bookingApp/static/js/2.eaf11853.chunk.js"
+  },
+  {
+    "revision": "7d4fc3edb17f3b9a1052e86af54b0e8e",
+    "url": "/React-slot-bookingApp/static/js/2.eaf11853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9a78a6adcb37c906231",
+    "url": "/React-slot-bookingApp/static/js/main.5b133079.chunk.js"
+  },
+  {
+    "revision": "556735281da71a886ad4",
+    "url": "/React-slot-bookingApp/static/js/runtime-main.ec8e5b35.js"
+  }
+]);
